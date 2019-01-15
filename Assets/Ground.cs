359082fe0eb20b6e8a1353696c9cb28f09168ac5
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-01-15 19:57:00 +1100

    hooked up pointer, ground and piloti

--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -5,21 +5,32 @@ using deVoid.Utils;
 
 public class Ground : MonoBehaviour {
 
-	public GameObject go;
+	public List<Vector3> points = new List<Vector3>();
+
 	private void OnEnable()
 	{
 		Signals.Get<Pointer.OnPointer>().AddListener(onPointer);
+		Signals.Get<Pointer.OnPointerUp>().AddListener(onPointerUp);
 	}
 
 	private void OnDisable()
 	{
 		Signals.Get<Pointer.OnPointer>().RemoveListener(onPointer);	
+		Signals.Get<Pointer.OnPointerUp>().RemoveListener(onPointerUp);
 	}
 
 	public void onPointer(Vector3 pos){
 
+		points.Add(pos);
 		Debug.DrawRay(pos, Vector3.up, Color.blue);
 		Debug.Log("Did Hit");
+	}
+
+	public void onPointerUp(Vector3 pos){
+		var go = new GameObject();
+		go.transform.position = pos;
+		var script = go.AddComponent<Piloti>();
+		script.points = points;
 	}
 
 	// Use this for initialization
--- Assets/Pointer.cs
+++ Assets/Pointer.cs
@@ -6,6 +6,7 @@ using deVoid.Utils;
 public class Pointer : MonoBehaviour {
 
 	public class OnPointer : ASignal<Vector3>{}
+	public class OnPointerUp : ASignal<Vector3>{}
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,7 @@ public class Pointer : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
+
 		if(Input.GetMouseButton(0)){
 			// Bit shift the index of the layer (8) to get a bit mask
 			int layerMask = 1 << 8;
@@ -29,6 +31,30 @@ public class Pointer : MonoBehaviour {
 			if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
 			{
 				Signals.Get<OnPointer>().Dispatch(hit.point);
+				Debug.DrawLine(ray.origin, hit.point, Color.blue);
+				// Debug.Log("Did Hit");
+			}
+			else
+			{
+				// Debug.DrawRay(ray.origin,ray.direction * 100 , Color.red);
+				// Debug.Log("Did not Hit");
+			}
+		}else if(Input.GetMouseButtonUp(0)){
+			// Bit shift the index of the layer (8) to get a bit mask
+			int layerMask = 1 << 8;
+
+			// This would cast rays only against colliders in layer 8.
+			// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+			layerMask = ~layerMask;
+
+			RaycastHit hit;
+			// Does the ray intersect any objects excluding the player layer
+			var mousePos = Input.mousePosition;
+			var cam = Camera.main;
+			Ray ray = cam.ScreenPointToRay(mousePos);
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+			{
+				Signals.Get<OnPointerUp>().Dispatch(hit.point);
 				Debug.DrawLine(ray.origin, hit.point, Color.blue);
 				// Debug.Log("Did Hit");
 			}
