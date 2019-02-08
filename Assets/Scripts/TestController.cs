a5116d9e5bcb2da7c2aa420b124631c4b47bf596
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-02-08 19:24:19 +1100

    upgraded to 2018.3, removed ar-core, removed lightweight render pipeline moved back to standard

--- Assets/Scripts/TestController.cs
+++ Assets/Scripts/TestController.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using deVoid.Utils;
-using GoogleARCore;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using deVoid.Utils;
+// using GoogleARCore;
 
-public class TestController : MonoBehaviour {
+// public class TestController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 		
-	}
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
+// 	// Update is called once per frame
+// 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.Space)){
+// 		if(Input.GetKeyDown(KeyCode.Space)){
 
-			Anchor anchor = new Anchor();
-			TrackableHit hit = new TrackableHit();
-			Signals.Get<AnchorCreated>().Dispatch(anchor,hit);
-		}
+// 			Anchor anchor = new Anchor();
+// 			TrackableHit hit = new TrackableHit();
+// 			Signals.Get<AnchorCreated>().Dispatch(anchor,hit);
+// 		}
 		
-	}
-}
+// 	}
+// }
