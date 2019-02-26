61d850fb99e3473b00f882dbd08aad91c5013b91
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-02-26 19:48:16 +1100

    moving things into states

--- Assets/Scripts/GroundPlanScene.cs
+++ Assets/Scripts/GroundPlanScene.cs
@@ -14,6 +14,7 @@ public class GroundPlanScene : MonoBehaviour {
 	public Vector3 d = Vector3.zero;
 	public bool ziggy = false;
 	public GroundPlan gp;
+	int state = 0;
 
 	private void OnEnable()
 	{
@@ -29,60 +30,77 @@ public class GroundPlanScene : MonoBehaviour {
 		Signals.Get<Pointer.OnPointerUp>().RemoveListener(onPointerUp);
 	}
 
+	private void Start()
+	{
+		state++;
+	}
+
+	private void Update()
+	{
+		
+	}
+
 	public void onPointerDown(RaycastHit hit){
 
-		points = new List<Vector3>();
-		a = Vector3.zero;
-		b = Vector3.zero;
-		c = Vector3.zero;	
-		ziggy = false;
-		if(gp){
-			GameObject.DestroyImmediate(gp.gameObject);
+		if(state == 1){
+			points = new List<Vector3>();
+			a = Vector3.zero;
+			b = Vector3.zero;
+			c = Vector3.zero;	
+			ziggy = false;
+			if(gp){
+				GameObject.DestroyImmediate(gp.gameObject);
+			}
 		}
 	}
 
 	public void onPointer(RaycastHit hit){
 
-		points.Add(hit.point);
+		if(state == 1){
+			points.Add(hit.point);
+		}
 	}
 
 	public void onPointerUp(RaycastHit hit){
 
-		a = points[0];
-		b = points[1];
-		foreach(var p in points){
-			if(Vector3.Distance(a, p) > Vector3.Distance(a,b)){
-				b = p;
+		if(state == 1){
+
+			a = points[0];
+			b = points[1];
+			foreach(var p in points){
+				if(Vector3.Distance(a, p) > Vector3.Distance(a,b)){
+					b = p;
+				}else{
+				}
+			}
+			foreach(var p in points){
+				if(Vector3.Distance(b, p) > Vector3.Distance(a,b)){
+					ziggy = true;
+					c = p;
+				}
+			}
+			if(ziggy){
+				distance = Vector3.Distance(b,c);
+				d = GetHalfway(b,c);
 			}else{
+				distance = Vector3.Distance(a,b);
+				d = GetHalfway(a,b);
 			}
+			density = points.Count;
+			var go = Resources.Load<GameObject>("Prefabs/GroundPlan");
+			go = Instantiate(go,d,Quaternion.identity);
+			gp = go.GetComponent<GroundPlan>();
+			gp.points = points;
+			float closestA = Modulor.GetClosestFromList(Modulor.redSeries, distance);
+			float closestB = Modulor.GetClosestFromList(Modulor.blueSeries, distance);
+			float closest = Modulor.GetClosest(closestA,closestB, distance);
+			gp.size = Vector3.one * closest;
+			gp.limit = density;
+			var pos = gp.transform.position;
+			pos.y += closest /2;
+			gp.transform.position = pos;
+			gp.rays = true;
 		}
-		foreach(var p in points){
-			if(Vector3.Distance(b, p) > Vector3.Distance(a,b)){
-				ziggy = true;
-				c = p;
-			}
-		}
-		if(ziggy){
-			distance = Vector3.Distance(b,c);
-			d = GetHalfway(b,c);
-		}else{
-			distance = Vector3.Distance(a,b);
-			d = GetHalfway(a,b);
-		}
-		density = points.Count;
-		var go = Resources.Load<GameObject>("Prefabs/GroundPlan");
-		go = Instantiate(go,d,Quaternion.identity);
-		gp = go.GetComponent<GroundPlan>();
-		gp.points = points;
-		float closestA = Modulor.GetClosestFromList(Modulor.redSeries, distance);
-		float closestB = Modulor.GetClosestFromList(Modulor.blueSeries, distance);
-		float closest = Modulor.GetClosest(closestA,closestB, distance);
-		gp.size = Vector3.one * closest;
-		gp.limit = density;
-		var pos = gp.transform.position;
-		pos.y += closest /2;
-		gp.transform.position = pos;
-		gp.rays = true;
 	}
 
 	public Vector3 GetHalfway(Vector3 a, Vector3 b){
@@ -107,12 +125,5 @@ public class GroundPlanScene : MonoBehaviour {
 		Gizmos.DrawCube(d, scale);
 	}
 
-	private void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.R)){
-			if(gp){
-				GameObject.Destroy(gp.gameObject);
-			}
-		}
-	}
+
 }
